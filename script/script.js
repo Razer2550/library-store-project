037fa9e21bb4dfb9053876de000848139b496f43
Razer2550 <razerwxvy2550@gmail.com>
2022-12-22 20:07:40 +0500

    add newsletter section with img and styles

--- script/script.js
+++ script/script.js
@@ -42,15 +42,22 @@ var swiper = new Swiper (".books-slider", {
         delay: 9500,
         disableOnInteraction: false,
     },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
     breakpoints: {
         0: {
             slidesPerView: 1,
         },
+        450: {
+            slidesPerView: 2
+        },
         768: {
-            slidesPerView: 2,
+            slidesPerView: 3,
         },
         1024: {
-            slidesPerView: 3,
+            slidesPerView: 4,
         },
     },
 });
